2b597b15186cc85f6bd4ed7e4ae4ca3cd50f7f52
myhaa <32893249+myhaa@users.noreply.github.com>
2021-06-25 18:08:09 +0800

    feat(ml): 增加模型训练代码
    20210625

---
+++ model_train/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 _*-
+"""
+Author: meiyunhe
+Email: meiyunhe@corp.netease.com
+Date: 2021/06/04
+File: __init__.py.py
+Software: PyCharm
+Description:
+
+* [sklearn api](https://scikit-learn.org/stable/modules/classes.html#module-sklearn.neighbors)
+* [sklearn linear model](https://scikit-learn.org/stable/modules/classes.html#module-sklearn.linear_model)
+* [sklearn svm](https://scikit-learn.org/stable/modules/classes.html#module-sklearn.svm)
+* [sklearn decision tree](https://scikit-learn.org/stable/modules/classes.html#module-sklearn.tree)
+* [sklearn ensemble method](https://scikit-learn.org/stable/modules/classes.html#module-sklearn.ensemble)
+* [xgboost](https://xgboost.readthedocs.io/en/latest/#)
+* [lightgbm](https://lightgbm.readthedocs.io/en/latest/index.html)
+* [sklearn clustering](https://scikit-learn.org/stable/modules/classes.html#module-sklearn.cluster)
+"""
